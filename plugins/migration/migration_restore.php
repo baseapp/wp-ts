cc25be659c3decfb789a4060157768f38593839b
Vikas Patial <vikas@baseapp.com>
2016-07-26 21:17:12 +0530

    Minor fix in backup

--- plugins/migration/migration_restore.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Meta Info
- * FILE_NAME: migration_restore.php
- * LABEL: Restore Wordpress from a local backup
- * LINK_MAIN: /migration/migration_restore
- *
- */
---
+++ plugins/migration/restore.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Meta Info
+ * FILE_NAME: restore.php
+ * LABEL: Restore Wordpress from a local backup
+ * LINK_MAIN: /migration/restore
+ *
+ */
+
+respond('POST', '/migration/restore', 'migration_restore');
+
+function migration_restore(TsRequest $request, TsResponse $response)
+{
+
+    $response->data->title = "Restore Wordpress From Backup";
+
+    if (isset($request->backup_path)) {
+
+        $backupPath = TS_ABSPATH.$request->backup_path;
+        $backup = file_get_contents($backupPath . 'backup.txt');
+
+        if($backup) {
+
+            $restorePart = 0;
+            if(isset($request->restore_part)) {
+                $restorePart = $request->restore_part;
+            }
+
+            $files = explode("\n",$backup);
+
+            if(isset($files[$restorePart])) {
+                list($restoreFile,$size) = explode(",",$files[$restorePart],2);
+                //downloadFile($fetchUrl.$fetchFile,$fetchPath.$fetchFile);
+
+                if(strstr($restoreFile,'sql')) {
+                    // Import into Database
+                } else if(strstr($restoreFile,'tar')) {
+                    // Restore wp_content
+                }
+
+                $response->data->form = true;
+                $response->data->formData = array(
+                    array('name'  => 'link', 'type'  => 'hidden', 'value' => $request->link),
+                    array('name'  => 'backup_path', 'type'  => 'hidden', 'value' => $request->backup_path),
+                    array('name'  => 'fetch_part', 'type'  => 'hidden', 'value' => $restorePart + 1)
+                );
+                $response->data->simpleData = "Restoring : ".$restoreFile;
+                $response->data->formSubmit = true;
+
+        } else {
+
+        }
+
+    }
+    else if (isset($request->restore_path)) {
+        $restorePath = TS_ABSPATH.$request->restore_path;
+        $backup = file_get_contents($restorePath . 'backup.txt');
+
+        if($backup) {
+
+            $totalFiles = 0;
+            $totalSize = 0;
+
+            $files = explode("\n",$backup);
+            foreach($files as $file) {
+                list($bfile,$bsize) = explode(',',$file,2);
+                $totalFiles += 1;
+                $totalSize += $bsize;
+            }
+
+            $response->data->form = true;
+            $response->data->simpleData = sprintf("Backup Found<br />Files : %d<br />Size : %.2f MB",$totalFiles, $totalSize / (1024*1024) );
+            $response->data->formData = array(
+                array('name'  => 'link', 'type'  => 'hidden', 'value' => $request->link),
+                array('name'  => 'backup_path', 'type'  => 'hidden', 'value' => $request->backup_path),
+                array('name'  => 'submit', 'type'  => 'submit', 'value' => 'Restore Backup')
+            );
+            $response->sendDataJson();
+
+        } else {
+            // Backup Not found
+
+            $response->data->simpleData = "No Backup Found";
+        }
+
+
+    } else {
+        $response->data->simpleData = "Enter path of backup directory";
+        $response->data->form = true;
+        $response->data->formData = array(
+            array('name'  => 'link', 'type'  => 'hidden', 'value' => $request->link),
+            array('name'  => 'restore_path', 'label' => 'Backup Path', 'type'  => 'text','hint'=>'Directory where the backup files exist.','value' => 'backups/'.TS_SECRET.'/'.date('Y-m-d',time()).'/'),
+            array('name'  => 'submit', 'type'  => 'submit', 'value' => 'Check Backup')
+        );
+
+    }
+
+    $response->sendDataJson();
+
+}
